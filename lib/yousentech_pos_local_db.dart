93006073cad7504f734a71779687a7d1068f5561
eemzain <hudazain55@gmail.com>
2025-01-22 14:48:40 +0300

    .

--- lib/yousentech_pos_local_db.dart
+++ lib/yousentech_pos_local_db.dart
@@ -1,1 +1,6 @@
+library yousentech_pos_local_db;
+export 'src/app_table_structure.dart';
+export 'src/create_local_db_tables.dart';
+export 'src/db_helper.dart';
+export 'src/general_local_db.dart';
 
